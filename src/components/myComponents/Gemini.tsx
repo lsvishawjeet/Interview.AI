2d10498a343e62b993ee7fc1e214c631c2437158
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-06-27 23:15:24 +0530

    form added

--- src/components/myComponents/Gemini.tsx
+++ src/components/myComponents/Gemini.tsx
@@ -8,10 +8,31 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { timeStamp } from "console";
-import { Textarea } from "@/components/ui/textarea"
-import markdownit from 'markdown-it';
+import { Textarea } from "@/components/ui/textarea";
+import markdownit from "markdown-it";
 import { UserDetailsForm } from "./userDetailsForm";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
+const formSchema = z.object({
+  education: z.string().min(2, {
+    message: "Cant be empty",
+  }),
+  experience: z.string(),
+  company: z.string().min(2, { message: "This cant be empty" }),
+  role: z.string().min(2, { message: "This is required field" }),
+  salary: z.string().min(2, { message: "This cant be empty" }),
+});
 
 type Message = {
   text: string;
@@ -24,8 +45,9 @@ function Gemini() {
   const [userInput, setUserInput] = useState("");
   const [chat, setChat] = useState<any>(null);
   const [error, setError] = useState<string | null>(null);
-  const md = markdownit()
-  const takeMeDownRef = useRef<HTMLDivElement>(null)
+  const [isStarted, setIsStart] = useState(false)
+  const md = markdownit();
+  const takeMeDownRef = useRef<HTMLDivElement>(null);
 
   const apiKey = "AIzaSyDuHnfldeQNzC-IgXoQTJK27bLw-_xvFQw";
   const genAI = new GoogleGenerativeAI(apiKey);
@@ -42,7 +64,7 @@ function Gemini() {
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
   };
-  
+
   useEffect(() => {
     const initChat = async () => {
       try {
@@ -60,13 +82,13 @@ function Gemini() {
     };
     initChat();
   }, []);
-  
+
   //scroll to down
-  useEffect(()=>{
-    if(takeMeDownRef.current) {
-      takeMeDownRef.current.scrollIntoView()
+  useEffect(() => {
+    if (takeMeDownRef.current) {
+      takeMeDownRef.current.scrollIntoView();
     }
-  },[message])
+  }, [message]);
 
   const handleSendButton = async () => {
     try {
@@ -98,18 +120,126 @@ function Gemini() {
       handleSendButton();
     }
   };
-  
-  const formatMessage=(msg:string)=>{
-    return md.render(msg)
+
+  const formatMessage = (msg: string) => {
+    return md.render(msg);
+  };
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      education: "",
+      experience: "none",
+      company: "",
+      role: "",
+      salary: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsStart(true)
+    try {
+
+      if (chat) {
+        const result = await chat.sendMessage(`Education: ${values.education}, Experience: ${values.experience}, Company: ${values.company}, Role: ${values.role}, Expected Package: ${values.salary} inr`);
+        const botMessage: Message = {
+          text: result.response.text(),
+          role: "bot",
+          timeStamp: new Date(),
+        };
+        setMessage((prevMessages) => [...prevMessages, botMessage]);
+      }
+    } catch (error) {
+      setError("Failed to send message. ");
+    }
+    console.log(JSON.stringify(values));
   }
-
-
   return (
-    <div className="h-100vh">
-      <div>
-      {/* <UserDetailsForm/> */}
+    <div className="h-100vh ">
+      <div className={isStarted?"hidden" :"h-[70vh] flex"}>
+        <div className="w-[100vw] flex justify-center align-middle items-center overflow-scroll scrollbar-none">
+          <div className="md:w-[30%] w-[90%] p-5 rounded-xl bg-slate-50">
+            <p className="text-center font-semibold text-lg">Fill Details</p>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-1"
+              >
+                <FormField
+                  control={form.control}
+                  name="education"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Education</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Btech CSE" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="experience"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Experience (if any)</FormLabel>
+                      <FormControl>
+                        <Input placeholder="3 years MERN stack" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="company"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Company</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Google" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="role"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Role</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Senior Developer" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="salary"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Expected Package (in INR)</FormLabel>
+                      <FormControl>
+                        <Input placeholder="35 lakh INR" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit">Start</Button>
+              </form>
+            </Form>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center">
+      <div className={isStarted?"flex justify-center" :"hidden"}>
         <div className="flex flex-col md:w-[50vw] w-[90vw] absolute bottom-0 md:mb-10 mb-4 overflow-y-scroll max-h-[80vh] scrollbar-none ">
           {message.map((msg, index) => (
             <div
@@ -121,10 +251,12 @@ function Gemini() {
               }
             >
               <div className="md:max-w-[80%] max-w-[90%] mb-1 md:mb-5 overflow-wrap p-2 bg-slate-100 rounded-xl text-gray-950">
-                <p className="pb-2" dangerouslySetInnerHTML={{
+                <p
+                  className="pb-2"
+                  dangerouslySetInnerHTML={{
                     __html: formatMessage(msg.text),
-                  }}>
-                </p>
+                  }}
+                ></p>
                 <p className="text-sm font-light">
                   {msg.role === "bot" ? "Interviewer" : "You"} -{" "}
                   {msg.timeStamp.toLocaleTimeString()}
@@ -141,7 +273,8 @@ function Gemini() {
               onKeyDown={handleKeyPress}
               onChange={(e) => setUserInput(e.target.value)}
               placeholder="Type your message..."
-              className="mr-2 max-h-56 rounded-lg p-4 active:no-underline"/>
+              className="mr-2 max-h-56 rounded-lg p-4 active:no-underline"
+            />
 
             <Button onClick={handleSendButton}>Submit</Button>
           </div>
